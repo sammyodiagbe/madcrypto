66ee1142fb5c1f6e59223ebe600d6111ab38b878
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-15 09:26:44 -0500

    added list of cryptos and currencies

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+List<String> cryptoCurrencies = [
+  'BTC',
+  'ETH',
+  'USDT',
+  'USDC',
+  'BNB',
+  'BUSD',
+  'XRP',
+  'ADA'
+];
+List<String> currencies = [
+  'USD',
+  'EUR',
+  'JPY',
+  'GBP',
+  'AUD',
+  'CAD',
+  'CNH',
+  'NGN'
+];
