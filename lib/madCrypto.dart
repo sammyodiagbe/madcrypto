00c61f37daba38f2a4496a993b6028d830f1c6a1
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-15 09:05:46 -0500

    initialized mad crypto app

---
+++ lib/madCrypto.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import "package:flutter/material.dart";
+
+class MadCrypto extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Madcrypto'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
