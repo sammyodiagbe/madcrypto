0aee3e6410ca7d8eebbb35cb9ff89f45f3a9c598
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-15 21:00:45 -0500

    initialialized dotenv file, to keep api keys secured

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@
 
 import "package:flutter/material.dart";
 import 'package:madcrypto/madCrypto.dart';
+import "package:flutter_dotenv/flutter_dotenv.dart";
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MadCryptoApp());
 }
 
